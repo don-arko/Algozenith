cb50c150929d11222e46e29bf282a3b00091eb66
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-03-03 17:15:07 +0530

    Implement integer swapping functions
    Added two implementations for swapping two integers.

---
+++ Problems/Phase_0/Week1/Swaping.cpp
@@ -0,0 +1,15 @@
+//Solution 1 : 
+pair<int,int> swapping(int a,int b){
+	swap(a,b);
+	return make_pair({a,b});
+}
+
+//Solution 2: swaping without using inbuilt func and any third variable 
+pair<int,int> swapping(int a,int b){
+	a=a+b;  //a stores the sum of a and b
+	b=a-b;  //sum - b gives the value of a into b
+	a=a-b;  //sum - value of a gives the value of b into a
+	return make_pair({a,b});
+}
+
+
