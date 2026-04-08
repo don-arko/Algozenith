f6a5b0f3a8a72f5894be607a005937f53924f8e3
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-04-08 13:19:04 +0530

    Update Registration AZ101 .cpp

--- AZ_Questions/STL/Registration AZ101 .cpp
+++ AZ_Questions/STL/Registration AZ101 .cpp
@@ -4,20 +4,20 @@ RegistrationAZ101 :
 
 #include<bits/stdc++.h>
 using namespace std;
-void solve(){
-    int n;cin>>n;
-    map<string,int> mp;
-    while(n--){
+void solve(){                       //O(nlogn)
+    int n;cin>>n;  
+    map<string,int> mp;             //O(1)
+    while(n--){                     //O(nlogn)
         string s;cin>>s;
-        if(mp.find(s)!=mp.end()) {
+        if(mp.find(s)!=mp.end()) {  //O(logn)
             cout<<s<<mp[s]<<'\n';mp[s]++;
         }
-        else{
+        else{                       //O(logn)
             mp[s]++;cout<<"OK\n";
         }
     }
 }
-int32_t main(){
+int32_t main(){                     //O(T* nlogn)   = 
     ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int tt;cin>>tt;
     while(tt--){
