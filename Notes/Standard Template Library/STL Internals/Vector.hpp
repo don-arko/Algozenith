254453a6b4fca584693e30abbd1228d2a5a3e7ea
Arko Barman <116270662+don-arko@users.noreply.github.com>
2026-05-03 16:05:29 +0530

    Update Vector.hpp

--- Notes/Standard Template Library/STL Internals/Vector.hpp
+++ Notes/Standard Template Library/STL Internals/Vector.hpp
@@ -1,104 +1,115 @@
-template<typename T>
-class Vector{
-public: 
-    T* data; //data is a ptr to base element of the dynamic array
-    size_t size;     //pos where to do curr push_back
-    size_t capacity; // size of the underlying array used
+#include <bits/stdc++.h>
+using namespace std;
 
-    void resize(size_t new_capacity){
-        T* new_data=new T[new_capacity]; //new dynamic array
+template <typename T>
+class MyVector
+{
+public:
+    T *data;
+    size_t size;
+    size_t capacity;
 
-        //copy the contents of the old array into the new
-        for(size_t i=0;i<size;i++){
-            new_data[i]=data[i];
-        }
-
-        //delete the old array
-        delete[] data;
-        
-        data=new_data;
-        //size remains unchanged
-        capacity=new_capacity;
-
+    // Constructor with initial capacity
+    MyVector() : size(0), capacity(4)
+    {
+        data = new T[capacity];
     }
 
-    //default constructor
-    Vector(){
-        size=0;
-        capacity=4;
-        data=new T[capacity]; // creates a dynamic array of size=capacity and returns its base addr which is stores in data
-    }
-
-    //destructor 
-    ~Vector(){ // called automatically by compiler when scope of stack obj v ends
-        //and we attached cleanup mem statement i.e. delete with that so our heap 
-        //cleanup is now automated
+    // Destructor
+    ~MyVector()
+    {
         delete[] data;
     }
 
-    //copy constructor
-    Vector(const Vector& other){
-        
-        size=other.size;
-        capacity=other.capacity;
-        data=new T[capacity];
-        
-        //copy the vector
-        for(size_t i=0;i<size;i++){
-            data[i]=other.data[i]; //use. operator cause at this point [] operator is not defined for vector other while other.data is a array for this [] are defined
+    // Copy Constructor
+    MyVector(const MyVector &other) : size(other.size), capacity(other.capacity)
+    {
+        data = new T[capacity];
+        for (size_t i = 0; i < size; ++i)
+        {
+            data[i] = other.data[i];
         }
     }
 
-    //Assignment operator
-    Vector& operator=(const Vector& other){
-        if(this!=&other){ //both the vectors have diff address which means they are diff vectors
+    // Assignment Operator
+    MyVector &operator=(const MyVector &other)
+    {
+        if (this != &other)
+        {
             delete[] data;
-            size=other.size;
-            capacity=other.capacity;
-            data=new T[capacity];
-
-            //copy the data
-            for(size_t i=0;i<size;i++){
-                data[i]=other.data[i];
+            size = other.size;
+            capacity = other.capacity;
+            data = new T[capacity];
+            for (size_t i = 0; i < size; ++i)
+            {
+                data[i] = other.data[i];
             }
         }
-        return *this; //return the curr vector obj 
+        return *this;
     }
 
-    //push_back
-    void push_back(const T& x){
-        if(size==capacity){
-            resize(2*capacity);
+    // Resize the vector
+    void resize(size_t new_size)
+    {
+        if (new_size > capacity)
+        {
+            T *new_data = new T[new_size];
+            for (size_t i = 0; i < size; ++i)
+            {
+                new_data[i] = data[i];
+            }
+
+            delete[] data;
+            data = new_data;
+            capacity = new_size;
         }
-        data[size++]=x;
     }
 
-    //pop_back
-    void pop_back(){
-        if(size>0) size--;
+    // Push an element to the back of the vector
+    void push_back(const T &value)
+    {
+        if (size == capacity)
+        {
+            resize(capacity * 2);
+        }
+        data[size] = value; // Assign value at the current size
+        size++;             // Increment size after assigning the value
     }
 
-    //random access
-    T& at(const size_t& idx){
-        if(idx>=size){
+    // Remove last element
+    void pop_back()
+    {
+        if (size > 0)
+        {
+            size--;
+        }
+    }
+
+    // Access element at given index
+    T &at(size_t index)
+    {
+        if (index >= size)
+        {
             throw std::out_of_range("Index out of range");
         }
-        return data[idx];
-    }
-    T& operator[](const size_t& idx){
-        if(idx>=size){
-            throw std::out_of_range("Index out of range");
-        }
-        return data[idx];
-    }
-
-    //size method
-    size_t _size(){
-        return size;
-    }
-
-    //clear methods
-    void clear(){
-        size=0;
+        return data[index];
     }
 };
+
+int main()
+{
+    // Example usage
+    MyVector<int> myVec;
+
+    myVec.push_back(1);
+    myVec.push_back(2);
+    myVec.push_back(3);
+    myVec.pop_back();
+    myVec.push_back(4);
+
+    cout << "Element at index 2: " << myVec.at(2) << endl;
+    cout << "Vector size: " << myVec.size << endl;
+    cout << "Vector capacity: " << myVec.capacity << endl;
+
+    return 0;
+}
